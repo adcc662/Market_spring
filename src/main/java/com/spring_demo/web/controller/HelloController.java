e2f79f09782630e7bf44861f7f50602a5797d579
Angel David <adcc662@gmail.com>
2023-11-15 11:02:23 -0600

    Initializing the app and inserting docker

---
+++ src/main/java/com/spring_demo/web/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.spring_demo.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/greet")
+    public String greet(){
+        return "Never stop learning";
+    }
+}
