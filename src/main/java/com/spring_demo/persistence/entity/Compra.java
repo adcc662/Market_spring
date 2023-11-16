b3429b32cf0af6f44a7fbc4ab534ebdaf08b3288
Angel David <adcc662@gmail.com>
2023-11-15 23:19:15 -0600

    Models, getters and setters and modifying dependencies to build docker compose

---
+++ src/main/java/com/spring_demo/persistence/entity/Compra.java
@@ -0,0 +1,73 @@
+package com.spring_demo.persistence.entity;
+import jakarta.persistence.*;
+import  java.time.LocalDateTime;
+
+@Entity
+@Table(name = "compras")
+public class Compra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_cliente")
+    private Integer idCliente;
+
+    private LocalDateTime fecha;
+
+    @Column(name = "medio_pago")
+    private String medio_pago;
+
+    private String comentario;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getMedio_pago() {
+        return medio_pago;
+    }
+
+    public void setMedio_pago(String medio_pago) {
+        this.medio_pago = medio_pago;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    private String estado;
+}
