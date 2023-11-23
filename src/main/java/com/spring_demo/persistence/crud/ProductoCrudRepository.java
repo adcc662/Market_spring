d5087414d86a6c67a505f520c81a23f394e94876
Angel David <adcc662@gmail.com>
2023-11-23 15:09:49 -0600

    Product repository finished and creating methods to self product

--- src/main/java/com/spring_demo/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/spring_demo/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,13 @@ package com.spring_demo.persistence.crud;
 import com.spring_demo.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findBycantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
+    Optional<List<Producto>> findBynombreAndEstado(String nombre, boolean estado);
+
 }
