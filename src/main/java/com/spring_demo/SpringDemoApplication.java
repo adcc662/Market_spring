c8ddb6a566b3df59fe97874885c41976b9c48bc6
Angel David <adcc662@gmail.com>
2023-11-30 17:04:53 -0600

    Fixing bugs and still solve the problem in JPA repository

--- src/main/java/com/spring_demo/SpringDemoApplication.java
+++ src/main/java/com/spring_demo/SpringDemoApplication.java
@@ -2,8 +2,12 @@ package com.spring_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.spring_demo.persistence.entity"})
+@EnableJpaRepositories(basePackages = {"com.spring_demo.persistence.repository"})
 public class SpringDemoApplication {
 
     public static void main(String[] args) {
