a9989f931864b2386dc356c0b407db61df0a3bec
Angel David <adcc662@gmail.com>
2023-12-02 13:23:59 -0600

    Service and controller of purchase

---
+++ src/main/java/com/spring_demo/domain/service/PurchaseService.java
@@ -0,0 +1,30 @@
+package com.spring_demo.domain.service;
+
+import com.spring_demo.domain.Purchase;
+import com.spring_demo.domain.repository.PurchaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PurchaseService {
+    @Autowired
+    private PurchaseRepository purchaseRepository;
+
+    public List<Purchase> getAll(){
+        return purchaseRepository.getAll();
+    }
+
+    public Optional<List<Purchase>> getByClient(String clientId){
+        return purchaseRepository.getByClient(clientId);
+
+    }
+
+    public Purchase save(Purchase purchase){
+        return purchaseRepository.save(purchase);
+    }
+
+
+}
