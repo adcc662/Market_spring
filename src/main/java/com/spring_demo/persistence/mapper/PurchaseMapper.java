8a7064f106f5276d38ee5d812c04bfcd5fe565ec
Angel David <adcc662@gmail.com>
2023-12-02 12:11:34 -0600

    Correction in purchase toPurchase

--- src/main/java/com/spring_demo/persistence/mapper/PurchaseMapper.java
+++ src/main/java/com/spring_demo/persistence/mapper/PurchaseMapper.java
@@ -7,6 +7,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring", uses = {PurchaseItemMapper.class})
 public interface PurchaseMapper {
 
